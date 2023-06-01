595c94c8c0b4edcfb5fd857ca91ebc5d8cf02c62
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-06-01 12:59:58 -0700

    feat: nix flake setup
    Add `flake.nix` (and friends) to help Nix user develop, compile
    and use this project.
    
    You can read more about Nix Flakes:
    
    https://www.tweag.io/blog/2020-05-25-flakes/

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Scalable push server for XMPP";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, crane }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        craneLib = crane.lib.${system};
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+
+          buildInputs = [
+            pkgs.openssl
+          ];
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+        };
+      in
+      {
+        packages.default = craneLib.buildPackage ({
+          meta = { mainProgram = "fpush"; };
+         } // commonArgs);
+
+        devShells = {
+          default = pkgs.mkShell {
+
+            buildInputs = [ ] ++ commonArgs.buildInputs;
+            nativeBuildInputs = builtins.attrValues
+              {
+                inherit (pkgs) cargo rustc nixpkgs-fmt shellcheck rnix-lsp;
+              } ++ [
+              # This is required to prevent a mangled bash shell in nix develop
+              # see: https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
+              (pkgs.hiPrio pkgs.bashInteractive)
+
+            ] ++ commonArgs.nativeBuildInputs;
+          };
+        };
+      }
+    );
+}
