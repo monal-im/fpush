d0b4306cfc8a830759d4920e8c3d370e288589ca
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2024-08-13 19:15:09 +0200

    bump nix to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Scalable push server for XMPP";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     crane = {
       url = "github:ipetkov/crane";
